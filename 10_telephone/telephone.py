650ef7a70cd5967960181281a756a28ed23b251a
ahr2042 <rashed-a@web.de>
2023-10-02 10:42:01 +0200

    project 10

--- 10_telephone/telephone.py
+++ 10_telephone/telephone.py
@@ -2,10 +2,13 @@
 """
 Author : A-Rashed <Rashed-a@web.de>
 Date   : 2023-10-02
-Purpose: Rock the Casbah
+Purpose: Randomly mutating strings
 """
 
 import argparse
+import os
+import random
+import string
 
 
 # --------------------------------------------------
@@ -13,40 +16,29 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Telephone", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument("text", metavar="text", help="Input text or file")
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
+    parser.add_argument(
+        "-s", "--seed", help="Random seed", metavar="seed", type=int, default=None
+    )
 
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
+    parser.add_argument(
+        "-m",
+        "--mutations",
+        help="Percent mutations",
+        metavar="mutations",
+        type=float,
+        default=0.1,
+    )
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.mutations >= 0.0 and args.mutations <= 1.0:
+        return args
+    else:
+        parser.error(f'--mutations "{args.mutations}" must be between 0 and 1')
 
 
 # --------------------------------------------------
@@ -54,19 +46,34 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    if os.path.isfile(args.text):
+        text = open(args.text).read().rstrip()
+    else:
+        text = args.text
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    random.seed(args.seed)
+
+    num_mutations = round(len(text) * args.mutations)
+    alpha = "".join(sorted(string.ascii_letters + string.punctuation))
+
+    print(f'You said: "{text}"')
+
+    # deterministic approach
+    indexes = random.sample(range(len(text)), num_mutations)
+    for i in indexes:
+        text = text[:i] + random.choice(alpha.replace(text[i], "")) + text[i + 1 :]
+
+    print(f'I heard : "{text}"')
+
+    # non-deterministic approach (faster and less mem usage for large text files)
+    """ 
+    new_text = ''
+    for char in text:
+        new_text += random.choice(alpha) if random.random() <= args.mutations else char
+    print(f'I heard : "{new_text}"')
+    """
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
